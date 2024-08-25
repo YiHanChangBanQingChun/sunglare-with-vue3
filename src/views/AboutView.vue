caae7d7be3e060cec9c73180935768e1376f6c53
YiHanChangBanQingChun <yihanchangbanqingchun@gmail.com>
2024-08-25 23:30:55 +0800

    生产版本4.2.1 1、修复了关于视图页面混乱的问题

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -22,7 +22,7 @@
         <ul>
           <li>Vue.js：项目的前端框架</li>
           <li>GeoScene API for JavaScript：项目的地图展示框架</li>
-          <li>PostgrSQL：项目的后端数据库</li>
+          <li>PostgreSQL：项目的后端数据库</li>
           <li>Echarts：项目的数据可视化框架</li>
           <li>Python Flask：项目的后端框架</li>
           <li class="image-container1"><img src="@/assets/kaifa1.png" alt="项目介绍图片" /></li>
